612d26fe2f1fa0c6591560f15c32ce7e257a3255
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-07-27 14:19:15 +0530

    fix: correct TextLoader import path and add debug print statement for document content

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning, module="langchain
 import os
 from dotenv import load_dotenv,find_dotenv
 from langchain_mistralai import ChatMistralAI
-from langchain_community import TextLoader
+from langchain_community.document_loaders import TextLoader
 
 load_dotenv(find_dotenv())
 
@@ -13,5 +13,7 @@ docs = TextLoader("Document loaders/notes.txt").load()
 model = ChatMistralAI(model="mistral-small-2506")
 
 result = model.invoke("hello")
+
+print(docs[0].page_content)
 
 print(result.content)
