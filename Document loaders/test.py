29bb26189eff7ebc9991106cad9050dd304418d2
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-07-27 15:27:42 +0530

    feat: add test script to verify LangChain document loader and character text splitting functionality

--- Document loaders/test.py
+++ Document loaders/test.py
@@ -1,6 +1,19 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+splitter = CharacterTextSplitter(
+    chunk_size= 10,
+    #separator="",
+    chunk_overlap=2
+)
+
 data = TextLoader("Document loaders/notes.txt")
 #print(data)
 
 docs = data.load()
-print(docs[0].page_content)
+
+chunks = splitter.split_documents(docs)
+
+#print(docs[0].page_content)
+print(len(chunks))
+print(chunks[0].page_content)
