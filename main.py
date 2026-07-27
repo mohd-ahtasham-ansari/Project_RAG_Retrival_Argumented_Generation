788d6860474bab0253e067e69f72a2a35fd80e21
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-07-27 16:35:50 +0530

    feat: implement RecursiveCharacterTextSplitter to chunk PDF documents for retrieval

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ from dotenv import load_dotenv,find_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv(find_dotenv())
 
 model = ChatMistralAI(model="mistral-small-2506")
 
-docs = PyPDFLoader("Document loaders/book.pdf").load()
+data = PyPDFLoader("Document loaders/book.pdf")
+docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+chunks = splitter.split_documents(docs)
+
 
 template = ChatPromptTemplate(
     [("system"," You are an AI that summarizes the text"),
