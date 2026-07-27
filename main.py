8b1db291186d4f51b5bb45cf1af9e684d2e40445
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-07-27 14:27:19 +0530

    feat: integrate ChatPromptTemplate to summarize document content using MistralAI

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import os
 from dotenv import load_dotenv,find_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv(find_dotenv())
 
 docs = TextLoader("Document loaders/notes.txt").load()
 
+template = ChatPromptTemplate(
+    [("system"," You are an AI that summarizes the text"),
+    ("human",{docs})]
+)
+
 model = ChatMistralAI(model="mistral-small-2506")
 
-result = model.invoke("hello")
+prompt =template.format_messages(data=docs[0].page_content)
 
-print(docs[0].page_content)
+result = model.invoke(prompt)
 
 print(result.content)
