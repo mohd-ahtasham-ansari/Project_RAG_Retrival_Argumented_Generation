24056b5705fbfda40dd6c59992741b51997abff9
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-07-27 14:17:17 +0530

    feat: initialize document loader with notes file

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from langchain_mistralai import ChatMistralAI
 from langchain_community import TextLoader
 
 load_dotenv(find_dotenv())
+
+docs = TextLoader("Document loaders/notes.txt").load()
+
 model = ChatMistralAI(model="mistral-small-2506")
 
 result = model.invoke("hello")
